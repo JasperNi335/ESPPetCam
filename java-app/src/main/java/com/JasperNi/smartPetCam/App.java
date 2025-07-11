050e082597c536397f94d12aed8e14f0ee76c432
Jasper Ni <jni335@aucklanduni.ac.nz>
2025-07-11 20:51:28 +1200

    Changes to Java-App to recieved json image files, and to handle image preview

--- java-app/src/main/java/com/JasperNi/smartPetCam/App.java
+++ java-app/src/main/java/com/JasperNi/smartPetCam/App.java
@@ -3,10 +3,15 @@ package com.JasperNi.smartPetCam;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
+import com.google.gson.Gson;
 
+import java.util.Base64;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.io.File;
+
 
 public class App {
     public static void main(String[] args) throws IOException{
@@ -14,6 +19,7 @@ public class App {
 
         server.createContext("/status", new StatusHandler());
         server.createContext("/upload", new UploadHandler());
+        server.createContext("/view_image", new ImageHandler());
 
         server.setExecutor(null); // default executor
         server.start();
@@ -38,9 +44,23 @@ public class App {
         public void handle(HttpExchange exchange) throws IOException{
             if ("POST".equals(exchange.getRequestMethod())){
                 String recieved = new String(exchange.getRequestBody().readAllBytes());
-                System.out.println("Recieved from ESP32: " + recieved);
 
-                String response = "Recieved";
+                // manage recieve JSON here
+                Gson gson = new Gson();
+                ImageJSONClass recievedImage = gson.fromJson(recieved, ImageJSONClass.class);
+
+                // decode image from base64
+                byte[] imageBytes = Base64.getDecoder().decode(recievedImage.getImage());
+                try (FileOutputStream fos = new FileOutputStream("recieved.jpg")){
+                    fos.write(imageBytes);
+                    fos.flush();
+                    System.out.println("Image saved as received.jpg");
+                }catch(IOException e){
+                    System.err.println("Failed to get image, Error: " + e.getMessage());
+                }
+
+                String response = "Image Recieved and Saved";
+                exchange.sendResponseHeaders(200, response.length());
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
                 os.flush();
@@ -49,5 +69,37 @@ public class App {
                 exchange.sendResponseHeaders(405, -1);
             }
         }   
+    }
+
+    static class ImageHandler implements HttpHandler{
+        public void handle(HttpExchange exchange) throws IOException{
+            // _______________________________________________
+            //
+            // DO LATER: MAKE THE IMAGE AUTO REFRESH/ IMPLEMENT VIDEO
+            // _______________________________________________
+            File imageFile = new File("recieved.jpg");
+
+            OutputStream os = exchange.getResponseBody();
+
+            if (!imageFile.exists()){
+                String response = "No Image found";
+                exchange.sendResponseHeaders(404, response.length());
+
+                os.write(response.getBytes());
+                os.flush();
+                os.close();
+                return;
+            }
+
+            byte[] image = java.nio.file.Files.readAllBytes(imageFile.toPath());
+
+            exchange.getResponseHeaders().add("Content-Type", "image/jpeg");
+            exchange.sendResponseHeaders(200, image.length);
+
+            os.write(image);
+            os.flush();
+            os.close();
+            return;
+        }
     }
 }
